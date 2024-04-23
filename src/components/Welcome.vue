24486c0b2f93e23b7d518d9b5664b41b0caa2d74
Tom Gregory <t.k.gregory@gmail.com>
2024-04-23 12:01:13 +0100

    Basic title input form.

---
+++ src/components/Welcome.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      title: ""
+    }
+  },
+}
+</script>
+
+<template>
+  <input v-model="title" placeholder="Video title" />
+
+  <div>
+    <p>{{ title }}</p>
+  </div>
+</template>
