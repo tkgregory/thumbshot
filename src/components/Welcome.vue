c160e0745342e6bbb95961a1e63241a44020a2eb
Tom Gregory <t.k.gregory@gmail.com>
2024-04-25 10:28:03 +0100

    Fix YouTube text styles to use hard coded rather than rem values.

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -10,15 +10,21 @@ export default {
 
 <template>
   <input v-model="title" placeholder="Video title" />
-  <div class="text-youtube-parent font-roboto">
-    <div class="font-medium text-youtube text-[#F1F1F1] leading-youtube mt-3 mb-1 overflow-ellipsis">
-      {{ title }}
+  <div class="flex">
+    <div class="mt-[12px] mr-[12px]">
+      <img class="rounded-full w-[48px] h-[48px]"
+        src="https://yt3.ggpht.com/KQZLfAGbguh1mszeZvkqLMkDHd-eDtFBPFMd3fMV_HuVTRQrTtK60sdqG7P7wlh8AvI0N1MjYg=s68-c-k-c0x00ffffff-no-rj" />
     </div>
-    <div class="flex flex-col text-[#AAAAAA] font-normal text-youtube">
-      <div>Some Channel Name</div>
-      <div>
-        <span>13K views</span>
-        <span class="before:content-['•'] before:mx-1">3 days ago</span>
+    <div class="text-youtube-parent font-roboto">
+      <div class="font-medium text-youtube text-[#F1F1F1] leading-youtube mt-[12px] mb-[4px] overflow-ellipsis">
+        {{ title }}
+      </div>
+      <div class="flex flex-col text-[#AAAAAA] font-normal text-youtube">
+        <div>Some Channel Name</div>
+        <div>
+          <span>13K views</span>
+          <span class="before:content-['•'] before:mx-1">3 days ago</span>
+        </div>
       </div>
     </div>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ export default {
         "youtube-parent": '10px',
       },
       lineHeight: {
-        'youtube': '2.2'
+        'youtube': '22px'
       }
     },
   },
