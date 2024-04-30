0a804032f4626e9d0d9b0056da63c1225fb01e87
Tom Gregory <t.k.gregory@gmail.com>
2024-04-30 15:53:07 +0100

    Tweak default preview and add tests.

---
+++ e2e/preview.spec.ts
@@ -0,0 +1,36 @@
+import { expect, test } from '@playwright/test';
+
+test('Shows default YouTube preview on load', async ({ page }) => {
+    await page.goto('/')
+    await expect(page.locator('div >> text="I made these 23 websites (and earned $562,943)"')).toHaveCount(1)
+});
+
+test('Update title', async ({ page }) => {
+    await page.goto('/')
+    await page.locator('input[name="title"]').fill('Updated title')
+    await expect(page.locator('div >> text="Updated title"')).toHaveCount(1)
+});
+
+test('Thumbnail too small', async ({ page }) => {
+    await page.goto('/')
+    await page.locator('input[name="thumbnail"]').setInputFiles(['./e2e/images/right-ratio-too-small.png']);
+    await expect(page.locator('div[role="alert"]')).toHaveText("Image size must be at least 1280x720 pixels")
+});
+
+test('Thumbnail wrong format', async ({ page }) => {
+    await page.goto('/')
+    await page.locator('input[name="thumbnail"]').setInputFiles(['./e2e/images/incorrect-format.txt']);
+    await expect(page.locator('div[role="alert"]')).toHaveText("Image must be one of these types: jpg, jpeg, png")
+});
+
+test('Thumbnail wrong aspect ratio', async ({ page }) => {
+    await page.goto('/')
+    await page.locator('input[name="thumbnail"]').setInputFiles(['./e2e/images/wrong-aspect-ratio.png']);
+    await expect(page.locator('div[role="alert"]')).toHaveText("Image aspect ratio must be 16:9")
+});
+
+test('Thumbnail can be oversized', async ({ page }) => {
+    await page.goto('/')
+    await page.locator('input[name="thumbnail"]').setInputFiles(['./e2e/images/oversized.png']);
+    await expect(page.locator('div[role="alert"]')).toHaveCount(0)
+});
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -4,8 +4,9 @@ const validExtensions = ['jpg', 'jpeg', 'png']
 export default {
   data() {
     return {
-      title: "Enter title here to see how it looks in YouTube even when the title is too long to fit on the screen",
-      file: ''
+      title: "I made these 23 websites (and earned $562,943)",
+      file: '',
+      errors: []
     }
   },
   methods: {
@@ -13,8 +14,9 @@ export default {
       if (!event.target.files[0]) {
         return
       }
+      const errors: string[] = this.errors
       if (validExtensions.indexOf(event.target.files[0].name.split('.').pop().toLowerCase()) == -1) {
-        alert(`Image must be one of these types: ${validExtensions.join(", ")}`)
+        errors.push(`Image must be one of these types: ${validExtensions.join(", ")}`)
         return
       }
 
@@ -24,15 +26,16 @@ export default {
         image.src = reader.result as string
         image.onload = function () {
           if (image.width / image.height != 16 / 9) {
-            alert("Image aspect ratio must be 16:9")
+            errors.push("Image aspect ratio must be 16:9")
             return
           }
           if (image.width < 1280 || image.height < 720) {
-            alert("Image size must be at least 1280x720 pixels")
+            errors.push("Image size must be at least 1280x720 pixels")
             return
           }
           var output = document.getElementById("output") as HTMLImageElement
           output.setAttribute("src", reader.result as string)
+          errors.length = 0
         }
       }
       reader.readAsDataURL(event.target.files[0])
@@ -45,9 +48,20 @@ export default {
   <div class="mb-8 flex flex-col gap-2">
     <label class="input input-bordered flex items-center gap-2">
       Title
-      <input v-model="title" type="text" class="grow" placeholder="Your video title" />
+      <input v-model="title" name="title" type="text" class="grow" placeholder="Your video title" />
     </label>
-    <input type="file" @change="onChangeImage($event)" class="file-input file-input-bordered w-full" />
+    <input name="thumbnail" type="file" @change="onChangeImage($event)" class="file-input file-input-bordered w-full" />
+    <template v-if="errors.length">
+      <div role="alert" class="alert alert-warning">
+        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+        </svg>
+        <ul>
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </div>
+    </template>
   </div>
   <div class="w-[400px] mx-2 mb-10 font-medium text-[12px] font-roboto">
     <div class="relative">
@@ -62,7 +76,7 @@ export default {
       <div class="flex">
         <div class="mt-[12px] mr-[12px] shrink-0">
           <img class="rounded-full w-[48px] h-[48px]"
-            src="https://yt3.ggpht.com/KQZLfAGbguh1mszeZvkqLMkDHd-eDtFBPFMd3fMV_HuVTRQrTtK60sdqG7P7wlh8AvI0N1MjYg=s68-c-k-c0x00ffffff-no-rj" />
+            src="https://yt3.ggpht.com/X0qndfJx9whZBlyyCwVvJw6vUk6wi7GdhemvveYlAUXyMBR_YrEMV-9U64m0T7h9qQ3OE46R=s88-c-k-c0x00ffffff-no-rj" />
         </div>
         <div class="text-youtube-parent">
           <div
