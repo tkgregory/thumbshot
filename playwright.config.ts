09c4c85b629d15d98fe967181d6ef49ccb8c79bf
Tom Gregory <t.k.gregory@gmail.com>
2024-04-30 16:46:40 +0100

    Try to get tests running on CI.

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,13 +8,12 @@ export default defineConfig({
     timeout: 1000
   },
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
     actionTimeout: 0,
     baseURL: 'http://localhost:5173',
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
 
   projects: [
