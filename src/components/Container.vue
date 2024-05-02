b235afeb2923af84e0e87c6272e194faa2dbbbcc
Tom Gregory <t.k.gregory@gmail.com>
2024-05-02 13:25:09 +0100

    Add swap arrow.

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -2,6 +2,7 @@
 import YouTubePreview from './YouTubePreview.vue'
 import { CirclePlus } from 'lucide-vue-next';
 import { CircleMinus } from 'lucide-vue-next';
+import { ArrowLeftRight } from 'lucide-vue-next';
 import { ref } from 'vue'
 </script>
 
@@ -17,11 +18,12 @@ export default {
 
 <template>
     <div class="flex gap-4 flex-wrap">
-        <template v-for="index in previewCount" :key="index">
+        <template v-for="index in previewCount">
             <YouTubePreview />
-            <div class="flex items-center">
+            <div class="flex flex-col gap-4 justify-center">
                 <CirclePlus v-if="previewCount == 1" @click="previewCount++" />
                 <CircleMinus v-if="previewCount == 2 && index == 1" @click="previewCount--" />
+                <ArrowLeftRight v-if="previewCount == 2 && index == 1" />
             </div>
         </template>
     </div>
