0f5e1244fbcef887bd6c2b342e6d4e10a8df315d
Tom Gregory <t.k.gregory@gmail.com>
2024-05-14 14:17:54 +0100

    Use hash in screenshot URL so can access page without navigation.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ScreenShotView from '../views/ScreenShotView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
