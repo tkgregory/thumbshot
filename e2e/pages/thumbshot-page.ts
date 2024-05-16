ec0a945c749ae5f7e4e06af965169d9bca6ed6f9
Tom Gregory <t.k.gregory@gmail.com>
2024-05-16 17:19:00 +0100

    Add delete preview test.

--- e2e/pages/thumbshot-page.ts
+++ e2e/pages/thumbshot-page.ts
@@ -10,4 +10,25 @@ export class ThumbShot {
         const fileChooser = await fileChooserPromise;
         await fileChooser.setFiles([`./e2e/images/${imageName}`]);
     }
+
+    async updateTitle(index: number, title: string) {
+        await this.page.locator(`youtube-preview:nth-child(${index + 1}) span[class="cursor-pointer"]`).first().click()
+        await this.page.locator('input[name="title"]').fill(title)
+        await this.page.locator('input[name="title"]').blur()
+    }
+
+    async updateChannelName(index: number, channelName: string) {
+        await this.page.locator(`youtube-preview:nth-child(${index + 1}) span[class="cursor-pointer"]`).nth(1).click()
+        await this.page.locator('input[name="channelName"]').fill(channelName)
+        await this.page.locator('input[name="channelName"]').blur()
+    }
+
+    getPreviewTitle(index: number) {
+        return this.page.locator(`youtube-preview:nth-child(${index + 1}) span`).first()
+    }
+
+    async clickDelete(index: number) {
+        await this.page.hover(`youtube-thumbnail:nth-child(${index + 1})`)
+        await this.page.locator('youtube-thumbnail div[data-tip="Delete this preview"]').nth(index).click()
+    }
 }
--- e2e/preview.spec.ts
+++ e2e/preview.spec.ts
@@ -16,17 +16,38 @@ test('Can add a thumbnail', async ({page, thumbshot}) => {
 test('Can update title', async ({page, thumbshot}) => {
     thumbshot.addThumbnail('oversized.png')
 
-    await page.locator('span[class="cursor-pointer"]').first().click()
-    await page.locator('input[name="title"]').fill('Updated title')
-    await page.locator('input[name="title"]').blur()
+    await thumbshot.updateTitle(0, 'Updated title')
     await expect(page.locator('span >> text="Updated title"')).toHaveCount(1)
+});
+
+test('Can update 2nd title', async ({page, thumbshot}) => {
+    thumbshot.addThumbnail('oversized.png')
+    await thumbshot.updateTitle(0, 'Preview 1')
+
+    thumbshot.addThumbnail('oversized.png')
+    await thumbshot.updateTitle(1, 'Preview 2')
+
+    await expect(page.locator('youtube-thumbnail')).toHaveCount(2)
+    await expect(thumbshot.getPreviewTitle(0)).toHaveText('Preview 1')
+    await expect(thumbshot.getPreviewTitle(1)).toHaveText('Preview 2')
 });
 
 test('Can update channel name', async ({page, thumbshot}) => {
     thumbshot.addThumbnail('oversized.png')
 
-    await page.locator('span[class="cursor-pointer"]').nth(1).click()
-    await page.locator('input[name="channelName"]').fill('Updated channel name')
-    await page.locator('input[name="channelName"]').blur()
+    await thumbshot.updateChannelName(0, 'Updated channel name')
     await expect(page.locator('span >> text="Updated channel name"')).toHaveCount(1)
+});
+
+test('Can delete preview', async ({page, thumbshot}) => {
+    thumbshot.addThumbnail('oversized.png')
+    await thumbshot.updateTitle(0, 'Preview 1')
+
+    thumbshot.addThumbnail('oversized.png')
+    await thumbshot.updateTitle(1, 'Preview 2')
+
+    await thumbshot.clickDelete(0)
+
+    await expect(page.locator('youtube-thumbnail')).toHaveCount(1)
+    await expect(thumbshot.getPreviewTitle(0)).toHaveText('Preview 2')
 });
