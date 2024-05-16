71898094498084484ac57766a8d08139f491cd84
Tom Gregory <t.k.gregory@gmail.com>
2024-05-16 10:09:48 +0100

    Decrease size of watermark.

--- src/views/ScreenShotView.vue
+++ src/views/ScreenShotView.vue
@@ -7,9 +7,9 @@ import ScreenshotYouTubeContainer from '../components/SreenshotYouTubeContainer.
         <screenshot-container class="block p-[16px]">
             <ScreenshotYouTubeContainer />
 
-            <div class="flex items-center justify-start gap-2 mt-8">
-                <img src="/logo.png" alt="logo" width="20" height="20" class="object-fill" />
-                <span class="text-xl">thumbshot.io</span>
+            <div class="flex items-center justify-start gap-1 mt-8">
+                <img src="/logo.png" alt="logo" width="15" height="15" class="object-fill" />
+                <span class="text-sm">thumbshot.io</span>
             </div>
         </screenshot-container>
     </main>
