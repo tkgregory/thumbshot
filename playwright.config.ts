108b40ed4ab9b018b08fbc1d5540ce6dd8735170
Tom Gregory <t.k.gregory@gmail.com>
2024-06-19 14:44:27 +0100

    Add missing permission.

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     timeout: 2 * 1000
   },
   forbidOnly: !!process.env.CI,
-  workers: 2,
+  workers: process.env.CI ? 2 : 1,
   reporter: 'html',
   use: {
     actionTimeout: 0,
