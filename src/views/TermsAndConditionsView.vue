01c2a4c3dc9469886463f5bd225873de0cec74ca
Tom Gregory <t.k.gregory@gmail.com>
2024-06-26 12:56:44 +0100

    Tweak terms.

--- src/views/TermsAndConditionsView.vue
+++ src/views/TermsAndConditionsView.vue
@@ -13,7 +13,7 @@ import { ArrowLeft } from 'lucide-vue-next';
         <h2 class="text-4xl py-8">Terms and conditions</h2>
         <ol class="p-4 list-decimal text-lg">
             <li>This site does not use cookies</li>
-            <li>Any thumbnails uploaded to thumbshot.io are deleted after 7 days</li>
+            <li>Screenshot thumbnails are deleted after 7 days</li>
             <li>thumbshot.io is owned and run by Tom Gregory Limited</li>
         </ol>
     </main>
